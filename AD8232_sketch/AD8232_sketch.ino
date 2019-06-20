56b3cb21a2003559fc2d6a57a4295ec0ac06fc8b
aspiringguru <bmatthewtaylor@gmail.com>
2019-06-20 11:47:48 +1000

    connected pins 32, 34, 35

--- AD8232_sketch/AD8232_sketch.ino
+++ AD8232_sketch/AD8232_sketch.ino
@@ -1,6 +1,10 @@
 
 const int OutputPin = 34;
+const int OutputPinLplus = 32;
+const int OutputPinLminus = 35;
 int Output = 0;
+int Lplus = 0;
+int Lminus = 0;
 
 
 void setup()
@@ -14,7 +18,13 @@ void setup()
 void loop()
 {
   Output = analogRead(OutputPin);
+  Lplus = analogRead(OutputPinLplus);
+  Lminus = analogRead(OutputPinLminus);
   Serial.print("Output:");
-  Serial.println(Output);  
+  Serial.print(Output);  
+  Serial.print("  Lplus :");
+  Serial.print(Lplus );  
+  Serial.print("  Lminus:");
+  Serial.println(Lminus);  
   delay(500);
 }
