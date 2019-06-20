5f2184ad082addcaf4679bd1d236ca12f609ade8
aspiringguru <bmatthewtaylor@gmail.com>
2019-06-20 11:20:36 +1000

    connected pin 34 to output, basic signal

--- AD8232_sketch/AD8232_sketch.ino
+++ AD8232_sketch/AD8232_sketch.ino
@@ -1,11 +1,20 @@
+
+const int OutputPin = 34;
+int Output = 0;
+
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("started");
+  delay(1000);
 }
 
 
 void loop()
 {
-  
+  Output = analogRead(OutputPin);
+  Serial.print("Output:");
+  Serial.println(Output);  
+  delay(500);
 }
